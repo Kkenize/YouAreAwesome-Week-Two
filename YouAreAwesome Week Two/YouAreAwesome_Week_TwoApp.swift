40366c31b5dd6a72e0510249df3626ca78295508
Kkenize <Kkenizez@gmail.com>
2025-01-26 16:41:41 -0500

    Initial Commit

---
+++ YouAreAwesome Week Two/YouAreAwesome_Week_TwoApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesome_Week_TwoApp.swift
+//  YouAreAwesome Week Two
+//
+//  Created by Zhejun Zhang on 1/26/25.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesome_Week_TwoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
