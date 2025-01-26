0dcc8c2b0264ae98bb32eafcc8d0f3a2a240c97e
Kkenize <Kkenizez@gmail.com>
2025-01-26 17:24:54 -0500

    Update 1 for week two!

--- YouAreAwesome Week Two/ContentView.swift
+++ YouAreAwesome Week Two/ContentView.swift
@@ -1,6 +1,6 @@
 //
 //  ContentView.swift
-//  YouAreAwesome Week Two
+//  YouAreAwesome
 //
 //  Created by Zhejun Zhang on 1/26/25.
 //
@@ -8,12 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var imageName = ""
+    @State private var message = ""
+    @State private var imageNumber = 0
     var body: some View {
+        let message1 = "You are awesome!"
+        let message2 = "You are great!"
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            Spacer()
+            
+            Image(imageName)
+                .resizable()
+                .scaledToFit()
+                .clipShape(RoundedRectangle(cornerRadius: 50))
+                .shadow(radius: 50)
+            Text(message)
+                .bold()
+                .font(.largeTitle)
+                .foregroundStyle(.yellow)
+            
+            Spacer()
+            
+            Button("Press Me!") {
+                //Note: imageName = "image\(imageNumber)" also works
+                imageName = "image" + String(imageNumber)
+//                imageName = (imageName == image1 ?9image2 : image1)
+                message = (message == message1 ? message2 : message1)
+//                imageNumber = imageNumber + 1
+                imageNumber += 1
+                if imageNumber > 9 {
+                    imageNumber = 0
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            .bold()
+            .font(.title2)
+            .tint(.orange)
         }
         .padding()
     }
